425ea41099b17301c76c890ccdbf84c75a68ac27
agrendra <agerf11@gmail.com>
2018-11-02 04:57:01 +0700

    Dubbing
    Tahapan Dubbing pada Permainan, banyak banget ambil refrensi dan masalah selanjutnya menyatukan algorima dari kesemua tips & trik yg ada

---
+++ File_PerEdukasi/Assets/Scripts/HalamanManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class HalamanManager : MonoBehaviour {
+
+	public bool isEscapeToExit;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp (KeyCode.Escape)){
+			if(isEscapeToExit){
+				Application.Quit();
+			}else{
+				MenuUtama();
+			}
+		}
+	}
+
+	public void KategoriPermainan(){
+		SceneManager.LoadScene("Kat_Game");
+	}
+
+	public void MenuUtama(){
+		SceneManager.LoadScene("Menu");
+	}
+
+	public void PermainanAngka(){
+		SceneManager.LoadScene("angka");
+	}
+
+	public void PermainanHuruf(){
+		SceneManager.LoadScene("huruf");
+	}
+}
---
+++ File_PerEdukasi/Assets/Scripts/SettingAudio.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingAudio : MonoBehaviour {
+
+	// Reference to Audio Source component
+    private AudioSource audioSrc;
+
+    // Music volume variable that will be modified
+    // by dragging slider knob
+    private float musicVolume = 1f;
+
+	// Use this for initialization
+	void Start () {
+
+        // Assign Audio Source component to control it
+        audioSrc = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Setting volume option of Audio Source to be equal to musicVolume
+        audioSrc.volume = musicVolume;
+	}
+
+    // Method that is called by slider game object
+    // This method takes vol value passed by slider
+    // and sets it as musicValue
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+
+    /*private static bool created = false;
+
+    void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+            Debug.Log("Awake: " + this.gameObject);
+        }
+    }
+
+    public void LoadScene()
+    {
+        if (SceneManager.GetActiveScene().name == "Menu")
+        {
+            SceneManager.LoadScene("Kat_Game", LoadSceneMode.Single);
+        }
+    }*/
+
+    /*private static SettingAudio instance = null;
+    public static SettingAudio Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }*/
+}
